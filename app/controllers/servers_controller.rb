b44b07cfbb886b645b90dbc826438465bc981b27
Richard Kent Jordan <rjordan@pobox.com>
2016-09-12 23:22:58 -0500

    Merge other project

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
 class ServersController < ApplicationController
+  def index
+    @servers = Server.find_all
+  end
+
+  def show
+    @server = params[:id]
+  end
 end
--- app/models/server.rb
+++ app/models/server.rb
@@ -1,19 +1,28 @@
+# frozen_string_literal: true
 class Server < ApplicationRecord
   ANSIBLE_RESULT_REGEX = /(?<hostname>.*) \| (?<status>.*) => (?<result>.*)/m
-  
+
   def call_ansible(action)
     result = `ansible #{action}`
     md = ANSIBLE_RESULT_REGEX.match(result)
-    vals = Hash[ md.names.zip( md.captures ) ]
-    vals['result']=JSON.parse(vals['result']) if vals['status']=='SUCCESS'
+    vals = Hash[md.names.zip(md.captures)]
+    vals['result'] = JSON.parse(vals['result']) if vals['status'] == 'SUCCESS'
   end
-  
+
   def facts
     @facts ||= call_ansible("-m setup #{name}")&.dig('ansible_facts') || {}
   end
-  
+
   def facts_loaded
     time = facts.dig('ansible_date_time', 'iso8601_micro')
-    time.blank? ? "UNKNOWN" : Time.parse(time)
+    time.blank? ? 'UNKNOWN' : Time.parse(time)
+  end
+
+  def self.client
+    @client ||= Elasticsearch::Client.new log: true, host: '192.168.0.202:9200'
+  end
+
+  def self.find_all
+    client.search(body: '{"size": 0, "aggs": { "hosts": {"terms": {"field": "host.raw", "size": 255}}}}').dig('aggregations', 'hosts', 'buckets').map { |v| v['key'] }
   end
 end
