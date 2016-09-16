27a2fe4d349c873061ba3bbad30078e5460cebad
Richard Kent Jordan <rjordan@pobox.com>
2016-09-15 22:22:08 -0500

    Adding a note with api docs

--- app/models/swarm.rb
+++ app/models/swarm.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 class Swarm < ApplicationRecord
+  # Docker API endpoint docs
+  # https://docs.docker.com/engine/reference/api/docker_remote_api_v1.24/#/list-nodes
 end
